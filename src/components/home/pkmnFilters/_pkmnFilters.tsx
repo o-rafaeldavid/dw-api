998662f599ea1a0d8926a927fe35d1651c82ea03
Rafael David <rafaelcapitaodavid@gmail.com>
2023-12-30 16:50:58 +0000

    home responsiva

--- src/components/home/pkmnFilters/_pkmnFilters.tsx
+++ src/components/home/pkmnFilters/_pkmnFilters.tsx
@@ -1,14 +1,20 @@
-import { LegacyRef, useRef } from "react";
+import { LegacyRef, useContext, useRef } from "react";
 import OrderBy from "./orderBy";
 import ThemeSelector from "./themeSelector";
 import WeightSlider from "./weightSlider";
 import { filterFormProps } from "../../../pages/home";
 import { TypesProps } from "../../../global/ts/_interfaces";
 import GenSelectors from "./generationSelectors";
+import { WindowDimensionContext } from "../../../contexts/windowResize";
+import { ThemeTypeContext } from "../../../contexts/themeType";
+import { iconsColorByName } from "../../../global/ts/icons";
 
 
 
 export default function PkmnFilters({stateSetter} : {stateSetter : React.Dispatch<filterFormProps>}){
+    
+    const {windowWidth} = useContext(WindowDimensionContext)
+    const {themeType} = useContext(ThemeTypeContext)
 
     let submitRef = useRef<HTMLButtonElement>()
 
@@ -31,7 +37,7 @@ export default function PkmnFilters({stateSetter} : {stateSetter : React.Dispatc
                 ) as HTMLInputElement[]
                 
                 if(typeSelecionado !== null){
-                    console.log(generationsSelecionados)
+                    if(windowWidth < 1200) form.removeAttribute('style')
                     stateSetter(
                         {
                             type: typeSelecionado.value.toLowerCase() as keyof TypesProps,
@@ -48,6 +54,29 @@ export default function PkmnFilters({stateSetter} : {stateSetter : React.Dispatc
             }}
         >
             <h2 style={{textAlign: 'center', marginBottom: '18px', color: 'black'}}>Filters</h2>
+            {(windowWidth < 1200) ? (
+                <svg id="cruz" x="0px" y="0px" viewBox="0 0 1080 1080"
+                    onClick={
+                        (e) => {
+                            const form = e.currentTarget.parentElement
+                            if(form !== null) form.removeAttribute('style')
+                        }
+                    }
+                >
+                    <rect
+                        style={{fill: `${iconsColorByName[themeType]}`}}
+                        x="0" y="450"
+                        transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 1303.6753 540)"
+                        width="1080" height="180"
+                    />
+                    <rect
+                        style={{fill: `${iconsColorByName[themeType]}`}}
+                        x="0" y="450"
+                        transform="matrix(0.7071 0.7071 -0.7071 0.7071 540 -223.6753)"
+                        width="1080" height="180"
+                    />
+                </svg>
+            ) : <></>}
             <br />
                 <h3>Name</h3>
                 <fieldset>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { CSSProperties, useContext, useEffect, useRef, useState } from 'react';
 import { useREQuery } from '../global/graphql/useREquery';
 
 import PkmnGrid from '../components/home/pkmnGrid/_pkmnGrid';
@@ -6,12 +6,46 @@ import PkmnNavigate from '../components/layout/pkmnNavigate';
 import PkmnFilters from '../components/home/pkmnFilters/_pkmnFilters';
 import { PaginaGridContext } from '../contexts/home/paginaGrid';
 import { ThemeTypeContext } from '../contexts/themeType';
+import { WindowDimensionContext } from '../contexts/windowResize';
 
-import { allTypes } from '../global/ts/icons';
+import { allTypes, iconsColorByName, iconsGradientColorsByName } from '../global/ts/icons';
 import { TypesProps } from '../global/ts/_interfaces';
 
 
 import '../scss/home.scss'
+
+
+
+function FilterIcon({style} : {style? : CSSProperties}){
+  return(
+    <svg x="0px" y="0px" viewBox="0 0 1080 1080">
+        <g>
+          <g>
+            <path style={style} d="M908.6,181H540C522,115.4,461.8,67,390.6,67S259.2,115.4,241.2,181h-70.7c-22.5,0-40.7,18.3-40.7,40.7
+              c0,22.5,18.3,40.7,40.7,40.7h70.7c17.9,65.6,78.1,114.1,149.4,114.1S522.1,328.1,540,262.5h368.7c22.5,0,40.7-18.3,40.7-40.7
+              C949.4,199.3,931.1,181,908.6,181z M390.6,295.1c-25.4,0-47.7-12.9-60.9-32.6c-7.9-11.7-12.5-25.6-12.5-40.7
+              c0-15.1,4.6-29.1,12.5-40.7c13.2-19.6,35.6-32.6,60.9-32.6s47.7,12.9,60.9,32.6c7.9,11.7,12.5,25.6,12.5,40.7
+              c0,15.1-4.6,29.1-12.5,40.7C438.3,282.2,415.9,295.1,390.6,295.1z"/>
+          </g>
+          <g>
+            <path style={style} d="M170.6,580.3h368.7c17.9,65.6,78.1,114.1,149.4,114.1S820.1,646,838.1,580.3h70.7c22.5,0,40.7-18.3,40.7-40.7
+              c0-22.5-18.3-40.7-40.7-40.7h-70.7c-17.9-65.6-78.1-114.1-149.4-114.1s-131.5,48.4-149.4,114.1H170.6c-22.5,0-40.7,18.3-40.7,40.7
+              C129.8,562.1,148.1,580.3,170.6,580.3z M688.6,466.2c25.4,0,47.7,12.9,60.9,32.6c7.9,11.7,12.5,25.6,12.5,40.7
+              c0,15.1-4.6,29.1-12.5,40.7c-13.2,19.6-35.6,32.6-60.9,32.6c-25.4,0-47.7-12.9-60.9-32.6c-7.9-11.7-12.5-25.6-12.5-40.7
+              c0-15.1,4.6-29.1,12.5-40.7C640.9,479.2,663.3,466.2,688.6,466.2z"/>
+          </g>
+          <g>
+            <path style={style} d="M908.6,816.6H540C522,751,461.8,702.5,390.5,702.5S259.1,751,241.1,816.6h-70.7c-22.5,0-40.7,18.3-40.7,40.7
+              c0,22.5,18.3,40.7,40.7,40.7h70.7c17.9,65.6,78.1,114.1,149.4,114.1S522,963.7,540,898.1h368.7c22.5,0,40.7-18.3,40.7-40.7
+              S931.1,816.6,908.6,816.6z M390.6,930.7c-25.4,0-47.7-12.9-60.9-32.6c-7.9-11.7-12.5-25.6-12.5-40.7c0-15.1,4.6-29.1,12.5-40.7
+              c13.2-19.6,35.6-32.6,60.9-32.6s47.7,12.9,60.9,32.6c7.9,11.7,12.5,25.6,12.5,40.7c0,15.1-4.6,29.1-12.5,40.7
+              C438.3,917.8,415.9,930.7,390.6,930.7z"/>
+          </g>
+        </g>
+    </svg>
+  )
+}
+
 
 const betweenCommas = (str : string) => `"${str}"`
 
@@ -33,7 +67,18 @@ export interface filterFormProps{
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 export default function Home() {
-  const pkmnsNumGrid = 6 * 2
+  const [pkmnsNumGrid, setPkmnsNumGrid] = useState<number>(6 * 2)
+  const {windowWidth, windowHeight} = useContext(WindowDimensionContext)
+  useEffect(
+    () => {
+      if(windowHeight <= 720 || windowWidth <= 1600) setPkmnsNumGrid(4 * 2)
+      else if(windowHeight > 720 && windowWidth > 1600) setPkmnsNumGrid(6 * 2)
+
+      if(windowWidth <= 1000) setPkmnsNumGrid(3 * 3)
+
+    }, [windowWidth, windowHeight]
+  )
+
   const { pagina, setPagina } = useContext(PaginaGridContext)
   const { themeType } = useContext(ThemeTypeContext)
 
@@ -67,13 +112,15 @@ export default function Home() {
       if(formState.direction !== formStateAnterior.direction) setOrder(formState.direction)
       if(formState.generations !== formStateAnterior.generations) setGenerations(`[${formState.generations.join(',')}]`)
 
+      console.log('change')
       setPagina(-Math.round(1000*Math.random()))
       setFormStateAnterior(formState)
-    }, [formState]
+    }, [formState, pkmnsNumGrid]
   )
 
   const queryReturn = (
     pg: number,
+    howMany: number,
     name: string,
     type: keyof TypesProps,
     direction : 'asc' | 'desc',
@@ -92,8 +139,8 @@ export default function Home() {
             }
           }
         },
-        limit: ${pkmnsNumGrid},
-        offset: ${pkmnsNumGrid * pg}
+        limit: ${howMany},
+        offset: ${howMany * pg}
     ){
       id
       name
@@ -124,9 +171,9 @@ export default function Home() {
     }
   `
 
-  const queryPkmn = useREQuery({ query: queryReturn(pagina, name, themeType, order, generations, weight) })
-  const queryPreviousPkmn = useREQuery({ query: queryReturn(pagina - 1, name, themeType, order, generations, weight) })
-  const queryNextPkmn = useREQuery({ query: queryReturn(pagina + 1, name, themeType, order, generations, weight) })
+  const queryPkmn = useREQuery({ query: queryReturn(pagina, pkmnsNumGrid, name, themeType, order, generations, weight) })
+  const queryPreviousPkmn = useREQuery({ query: queryReturn(pagina - 1, pkmnsNumGrid, name, themeType, order, generations, weight) })
+  const queryNextPkmn = useREQuery({ query: queryReturn(pagina + 1, pkmnsNumGrid, name, themeType, order, generations, weight) })
 
   let onMountRef = useRef(true)
 
@@ -143,12 +190,12 @@ export default function Home() {
         return
       }
       else if(pagina >= 0){
-        queryPkmn.newQuery(({ query: queryReturn(pagina, name, themeType, order, generations, weight) }))
+        queryPkmn.newQuery(({ query: queryReturn(pagina, pkmnsNumGrid, name, themeType, order, generations, weight) }))
 
         setExistPkmnAnteriores(false)
         setExistPkmnProximos(false)
-        queryPreviousPkmn.newQuery(({ query: queryReturn(pagina - 1, name, themeType, order, generations, weight) }))
-        queryNextPkmn.newQuery(({ query: queryReturn(pagina + 1, name, themeType, order, generations, weight) }))
+        queryPreviousPkmn.newQuery(({ query: queryReturn(pagina - 1, pkmnsNumGrid, name, themeType, order, generations, weight) }))
+        queryNextPkmn.newQuery(({ query: queryReturn(pagina + 1, pkmnsNumGrid, name, themeType, order, generations, weight) }))
         return
       }
       else{
@@ -192,6 +239,26 @@ export default function Home() {
   return (  
     <>
       <div id="interface">
+
+        {(windowWidth <= 1200) ? (
+          <div
+            className='showMobile' id="filterIcon"
+            onClick={
+              () => {
+                const pkmnFilters = document.getElementById('pkmnFilters')
+                if(pkmnFilters !== null){
+                  pkmnFilters.style.opacity = 1 + ''
+                  pkmnFilters.style.zIndex = 10 + ''
+                }
+              }
+            }
+          >
+            <FilterIcon style={{fill: `${iconsGradientColorsByName[themeType][0]}`}}/>
+            <FilterIcon style={{fill: `white`}}/>
+          </div>
+        ): <></>}
+
+
         <PkmnFilters stateSetter={setFormState}/>
         <div id="PkmnList">
           {
@@ -209,8 +276,8 @@ export default function Home() {
               />
             </>
           }
-            
         </div>
+        {(windowWidth <= 1200) ? <div className='showMobile'></div> : <></>}
       </div>
     </>
   )
